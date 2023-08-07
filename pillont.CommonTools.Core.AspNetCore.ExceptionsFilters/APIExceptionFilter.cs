302a32cb455908bc3ef04f1b1982e748ac344292
Thibaut PILLON <thibaut@deephawk.ai>
2023-08-07 16:52:47 +0200

    add API exception filter

---
+++ pillont.CommonTools.Core.AspNetCore.ExceptionsFilters/APIExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using pillont.CommonTools.Core.AspNetCore.Core.Exceptions;
+
+namespace pillont.CommonTools.Core.AspNetCore.ExceptionsFilters;
+
+public class APIExceptionFilter : IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        // Nothing to do
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        if (context.Exception is not APIException apiException)
+        {
+            return;
+        }
+
+        context.Result = new ObjectResult(apiException.ErrorBody) 
+        { 
+            StatusCode = apiException.StatusCode 
+        };
+        context.ExceptionHandled = true;
+    }
+}
