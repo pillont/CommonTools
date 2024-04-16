db581c6fad4b699be7230b3edd2416f63786cd90
Thibaut PILLON <thibaut@deephawk.ai>
2024-04-16 11:09:38 +0200

    update foreachasync

--- pillont.CommonTools.Core/Parallel/ForeachAsyncExtension.cs
+++ pillont.CommonTools.Core/Parallel/ForeachAsyncExtension.cs
@@ -5,21 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using pillont.CommonTools.Core.Enumerables;
 
-namespace pillont.CommonTools.Core.Parallel
-{
-    public static class ForeachAsyncExtension
-    {
-        /// <summary>
-        /// lance plusieurs collectes asynchrone en parallèle et agglomère les resultats en une seule liste
-        /// </summary>
-        public static async Task<IReadOnlyCollection<TResult>> AggregateForeachAsync<TElement, TResult>(this IEnumerable<TElement> collection, Func<TElement, Task<IEnumerable<TResult>>> action)
-        {
-            var allLists = await ForeachAsync(collection, action);
-            return allLists.AsParallel()
-                            .SelectMany(list => list.AsNotNull())
-                            .ToList();
-        }
+namespace pillont.CommonTools.Core.Parallel;
 
+public static class ForeachAsyncExtension
+{
+    /// <summary>
+    /// lance plusieurs collectes asynchrone en parallèle et agglomère les resultats en une seule liste
+    /// </summary>
+    public static async Task<IReadOnlyCollection<TResult>> AggregateForeachAsync<TElement, TResult>(this IEnumerable<TElement> collection, Func<TElement, Task<IEnumerable<TResult>>> action)
+    {
+        var allLists = await ForeachAsync(collection, action);
+        return allLists.AsParallel()
+                        .SelectMany(list => list.AsNotNull())
+                        .ToList();
+    }
+
+    /// <summary>
+    /// fait un foreach asynchrone sur une liste
+    /// </summary>
+    /// <typeparam name="TElement">type dans la liste</typeparam>
+    /// <param name="collection">source</param>
+    /// <param name="action">action async à appliquer sur chaque element de la liste</param>
+    /// <returns></returns>
+    public static async Task ForeachAsync<TElement>(
+        this IEnumerable<TElement> collection,
+        Func<TElement, Task> action)
+    {
+        await collection.ForeachAsync((e, i) => action(e));
+    }
         /// <summary>
         /// fait un foreach asynchrone sur une liste
         /// </summary>
@@ -28,72 +41,74 @@ namespace pillont.CommonTools.Core.Parallel
         /// <param name="action">action async à appliquer sur chaque element de la liste</param>
         /// <returns></returns>
         public static async Task ForeachAsync<TElement>(
-            this IEnumerable<TElement> collection, 
-            Func<TElement, Task> action)
+            this IEnumerable<TElement> collection,
+            Func<TElement, int, Task> action)
         {
             var errors = new ConcurrentBag<Exception>();
-            await UnsafeForeachAsync(collection, async e =>
+        await UnsafeForeachAsync(collection, async (e, i) =>
+        {
+            try
             {
-                try
-                {
-                    await action(e);
-                }
-                catch (Exception ex)
-                {
-                    errors.Add(ex);
-                }
-            });
-
-            if (errors.Any())
-            {
-                throw new AggregateException("errors during foreach async", errors);
+                await action(e, i);
             }
-        }
-
-        /// <summary>
-        /// fait un foreach asynchrone sur une liste
-        /// récupère tous les résultats et les retournes dans une liste
-        /// </summary>
-        /// <typeparam name="TElement">type dans la liste source</typeparam>
-        /// <typeparam name="TResult">type dans la liste résultante</typeparam>
-        /// <param name="collection">source</param>
-        /// <param name="action">action async à appliquer sur chaque element de la liste</param>
-        /// <returns>liste de tous les résultats des actions exécutées sur chaque éléments de la liste source</returns>
-        public static async Task<IReadOnlyCollection<TResult>> ForeachAsync<TElement, TResult>(
-            this IEnumerable<TElement> collection, 
-            Func<TElement, Task<TResult>> action)
-        {
-            var result = new ConcurrentBag<TResult>();
-
-            await ForeachAsync(collection, async e =>
+            catch (Exception ex)
             {
-                var r = await action(e);
-                result.Add(r);
-            });
+                errors.Add(ex);
+            }
+        });
 
-            return result;
-        }
-
-        /// <summary>
-        /// fait un foreach asynchrone sur une liste
-        /// récupère tous les résultats et les retournes dans une liste
-        /// </summary>
-        /// <typeparam name="TElement">type dans la liste source</typeparam>
-        /// <typeparam name="TResult">type dans la liste résultante</typeparam>
-        /// <param name="collection">source</param>
-        /// <param name="action">action async à appliquer sur chaque element de la liste</param>
-        /// <returns>liste de tous les résultats des actions exécutées sur chaque éléments de la liste source</returns>
-        public static async Task<IReadOnlyCollection<TResult>> ForeachAsync<TElement, TResult>(
-            this IEnumerable<TElement> collection,
-            Func<TElement, TResult> action)
+        if (errors.Any())
         {
-            return await collection.ForeachAsync(t=> Task.FromResult(action(t)));
+            throw new AggregateException("errors during foreach async", errors);
         }
+    }
 
-        private static async Task UnsafeForeachAsync<TElement>(this IEnumerable<TElement> collection, Func<TElement, Task> action)
+    /// <summary>
+    /// fait un foreach asynchrone sur une liste
+    /// récupère tous les résultats et les retournes dans une liste
+    /// </summary>
+    /// <typeparam name="TElement">type dans la liste source</typeparam>
+    /// <typeparam name="TResult">type dans la liste résultante</typeparam>
+    /// <param name="collection">source</param>
+    /// <param name="action">action async à appliquer sur chaque element de la liste</param>
+    /// <returns>liste de tous les résultats des actions exécutées sur chaque éléments de la liste source</returns>
+    public static async Task<IReadOnlyCollection<TResult>> ForeachAsync<TElement, TResult>(
+        this IEnumerable<TElement> collection, 
+        Func<TElement, Task<TResult>> action)
+    {
+        var result = new ConcurrentBag<KeyValuePair<int, TResult>>();
+
+        await ForeachAsync(collection, async (e,i) =>
         {
-            var allTasks = collection.Select(e => action(e));
-            await Task.WhenAll(allTasks);
-        }
+            var r = await action(e);
+            result.Add(new KeyValuePair<int, TResult>(i, r));
+        });
+
+        return result
+            .OrderBy(r=> r.Key)
+            .Select(r=> r.Value)
+            .ToList();
+    }
+
+    /// <summary>
+    /// fait un foreach asynchrone sur une liste
+    /// récupère tous les résultats et les retournes dans une liste
+    /// </summary>
+    /// <typeparam name="TElement">type dans la liste source</typeparam>
+    /// <typeparam name="TResult">type dans la liste résultante</typeparam>
+    /// <param name="collection">source</param>
+    /// <param name="action">action async à appliquer sur chaque element de la liste</param>
+    /// <returns>liste de tous les résultats des actions exécutées sur chaque éléments de la liste source</returns>
+    public static async Task<IReadOnlyCollection<TResult>> ForeachAsync<TElement, TResult>(
+        this IEnumerable<TElement> collection,
+        Func<TElement, TResult> action)
+    {
+        return await collection.ForeachAsync(t=> Task.FromResult(action(t)));
+    }
+
+    private static async Task UnsafeForeachAsync<TElement>(this IEnumerable<TElement> collection, Func<TElement, int, Task> action)
+    {
+        var allTasks = collection.Select((e, i) => action(e, i));
+        await Task.WhenAll(allTasks);
     }
 }
