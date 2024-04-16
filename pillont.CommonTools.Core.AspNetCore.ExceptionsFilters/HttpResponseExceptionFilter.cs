8f6afad95afb92e5b6aeec8e508d0998c8fb07f6
Thibaut PILLON <thibaut@deephawk.ai>
2024-04-16 11:09:43 +0200

    allow filter override

--- pillont.CommonTools.Core.AspNetCore.ExceptionsFilters/HttpResponseExceptionFilter.cs
+++ pillont.CommonTools.Core.AspNetCore.ExceptionsFilters/HttpResponseExceptionFilter.cs
@@ -1,49 +1,48 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 
-namespace pillont.CommonTools.Core.AspNetCore.ExceptionsFilters
+namespace pillont.CommonTools.Core.AspNetCore.ExceptionsFilters;
+
+/// <summary>
+/// Filtre pour transformer les <see cref="TException"/> en reponse HTTP appropriée : <see cref="TResult"/>
+///
+/// SOURCE : https://docs.microsoft.com/en-us/aspnet/core/web-api/handle-errors?view=aspnetcore-3.0
+/// </summary>
+public class HttpResponseExceptionFilter<TException, TResult> : IActionFilter
+    where TException : Exception
+    where TResult : ObjectResult
 {
-    /// <summary>
-    /// Filtre pour transformer les <see cref="TException"/> en reponse HTTP appropriée : <see cref="TResult"/>
-    ///
-    /// SOURCE : https://docs.microsoft.com/en-us/aspnet/core/web-api/handle-errors?view=aspnetcore-3.0
-    /// </summary>
-    public class HttpResponseExceptionFilter<TException, TResult> : IActionFilter
-        where TException : Exception
-        where TResult : ObjectResult
+    public void OnActionExecuted(ActionExecutedContext context)
     {
-        public void OnActionExecuted(ActionExecutedContext context)
+        TryFormatCustomException(context);
+
+        // HERE : action to apply after request
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        // HERE : action to apply before request
+    }
+
+    protected virtual object GetContent(TException exception)
+    {
+        return exception is IContentHolder contentHolder ? contentHolder.Content : exception.Message;
+    }
+
+    private void TryFormatCustomException(ActionExecutedContext context)
+    {
+        if (context.Exception is TException exception)
         {
-            TryFormatCustomException(context);
-
-            // HERE : action to apply after request
+            var content = GetContent(exception);
+            context.Result = GetResultObject(content);
+            context.ExceptionHandled = true;
+            return;
         }
+    }
 
-        public void OnActionExecuting(ActionExecutingContext context)
-        {
-            // HERE : action to apply before request
-        }
-
-        private object GetContent(TException exception)
-        {
-            if (exception is IContentHolder contentHolder)
-            {
-                return contentHolder.Content;
-            }
-
-            return exception.Message;
-        }
-
-        private void TryFormatCustomException(ActionExecutedContext context)
-        {
-            if (context.Exception is TException notExistsInStoreException)
-            {
-                object content = GetContent(notExistsInStoreException);
-                context.Result = (TResult)Activator.CreateInstance(typeof(TResult), content);
-                context.ExceptionHandled = true;
-                return;
-            }
-        }
+    protected virtual TResult GetResultObject(object content)
+    {
+        return (TResult)Activator.CreateInstance(typeof(TResult), content);
     }
 }
