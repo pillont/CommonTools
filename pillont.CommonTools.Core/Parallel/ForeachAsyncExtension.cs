b142c5fdf41fac140d04e77ed77ea769c80f7889
Thibaut PILLON <thibaut@deephawk.ai>
2024-07-01 07:12:17 +0200

    add cancellation token

--- pillont.CommonTools.Core/Parallel/ForeachAsyncExtension.cs
+++ pillont.CommonTools.Core/Parallel/ForeachAsyncExtension.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using pillont.CommonTools.Core.Enumerables;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using pillont.CommonTools.Core.Enumerables;
 
 namespace pillont.CommonTools.Core.Parallel;
 
@@ -12,9 +13,12 @@ public static class ForeachAsyncExtension
     /// <summary>
     /// lance plusieurs collectes asynchrone en parallèle et agglomère les resultats en une seule liste
     /// </summary>
-    public static async Task<IReadOnlyCollection<TResult>> AggregateForeachAsync<TElement, TResult>(this IEnumerable<TElement> collection, Func<TElement, Task<IEnumerable<TResult>>> action)
+    public static async Task<IReadOnlyCollection<TResult>> AggregateForeachAsync<TElement, TResult>(
+        this IEnumerable<TElement> collection,
+        Func<TElement, Task<IEnumerable<TResult>>> action,
+        CancellationToken? cancellationToken = null)
     {
-        var allLists = await ForeachAsync(collection, action);
+        var allLists = await ForeachAsync(collection, action, cancellationToken);
         return allLists.AsParallel()
                         .SelectMany(list => list.AsNotNull())
                         .ToList();
@@ -29,22 +33,25 @@ public static class ForeachAsyncExtension
     /// <returns></returns>
     public static async Task ForeachAsync<TElement>(
         this IEnumerable<TElement> collection,
-        Func<TElement, Task> action)
+        Func<TElement, Task> action,
+        CancellationToken? cancellationToken = null)
+
     {
-        await collection.ForeachAsync((e, i) => action(e));
+        await collection.ForeachAsync((e, i) => action(e), cancellationToken);
     }
-        /// <summary>
-        /// fait un foreach asynchrone sur une liste
-        /// </summary>
-        /// <typeparam name="TElement">type dans la liste</typeparam>
-        /// <param name="collection">source</param>
-        /// <param name="action">action async à appliquer sur chaque element de la liste</param>
-        /// <returns></returns>
-        public static async Task ForeachAsync<TElement>(
-            this IEnumerable<TElement> collection,
-            Func<TElement, int, Task> action)
-        {
-            var errors = new ConcurrentBag<Exception>();
+    /// <summary>
+    /// fait un foreach asynchrone sur une liste
+    /// </summary>
+    /// <typeparam name="TElement">type dans la liste</typeparam>
+    /// <param name="collection">source</param>
+    /// <param name="action">action async à appliquer sur chaque element de la liste</param>
+    /// <returns></returns>
+    public static async Task ForeachAsync<TElement>(
+        this IEnumerable<TElement> collection,
+        Func<TElement, int, Task> action,
+        CancellationToken? cancellationToken = null)
+    {
+        var errors = new ConcurrentBag<Exception>();
         await UnsafeForeachAsync(collection, async (e, i) =>
         {
             try
@@ -55,7 +62,8 @@ public static class ForeachAsyncExtension
             {
                 errors.Add(ex);
             }
-        });
+        },
+        cancellationToken);
 
         if (errors.Any())
         {
@@ -73,20 +81,21 @@ public static class ForeachAsyncExtension
     /// <param name="action">action async à appliquer sur chaque element de la liste</param>
     /// <returns>liste de tous les résultats des actions exécutées sur chaque éléments de la liste source</returns>
     public static async Task<IReadOnlyCollection<TResult>> ForeachAsync<TElement, TResult>(
-        this IEnumerable<TElement> collection, 
-        Func<TElement, Task<TResult>> action)
+        this IEnumerable<TElement> collection,
+        Func<TElement, Task<TResult>> action,
+        CancellationToken? cancellationToken = null)
     {
         var result = new ConcurrentBag<KeyValuePair<int, TResult>>();
 
-        await ForeachAsync(collection, async (e,i) =>
+        await ForeachAsync(collection, async (e, i) =>
         {
             var r = await action(e);
             result.Add(new KeyValuePair<int, TResult>(i, r));
-        });
+        }, cancellationToken);
 
         return result
-            .OrderBy(r=> r.Key)
-            .Select(r=> r.Value)
+            .OrderBy(r => r.Key)
+            .Select(r => r.Value)
             .ToList();
     }
 
@@ -101,14 +110,21 @@ public static class ForeachAsyncExtension
     /// <returns>liste de tous les résultats des actions exécutées sur chaque éléments de la liste source</returns>
     public static async Task<IReadOnlyCollection<TResult>> ForeachAsync<TElement, TResult>(
         this IEnumerable<TElement> collection,
-        Func<TElement, TResult> action)
+        Func<TElement, TResult> action,
+        CancellationToken? cancellationToken = null)
     {
-        return await collection.ForeachAsync(t=> Task.FromResult(action(t)));
+        return await collection.ForeachAsync(t => Task.FromResult(action(t)), cancellationToken);
     }
 
-    private static async Task UnsafeForeachAsync<TElement>(this IEnumerable<TElement> collection, Func<TElement, int, Task> action)
+    private static async Task UnsafeForeachAsync<TElement>(
+        this IEnumerable<TElement> collection,
+        Func<TElement, int, Task> action,
+        CancellationToken? cancellationToken = null)
     {
         var allTasks = collection.Select((e, i) => action(e, i));
-        await Task.WhenAll(allTasks);
+
+        await Task.WhenAll(allTasks).WaitAsync(
+            cancellationToken ?? CancellationToken.None
+        );
     }
 }
